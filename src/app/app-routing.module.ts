6cbd80ef0c99c13fb9318958632cec620fb541e6
ilyan <ilyan.kortbi.ifcha@gmail.com>
2022-09-05 17:29:36 +0200

    first init

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './page/home/home.component';
+import { ProgrammeComponent } from './page/Programme/programme.component';
+import { AboutComponent } from './page/about/about.component';
+import { ContactComponent } from './page/contact/contact.component';
+
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'programme',component:ProgrammeComponent},
+  {path:'about',component:AboutComponent},
+  {path:'contact',component:ContactComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
